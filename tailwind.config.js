12b32c40ecefc02b20df16cdde6913104aff09e1
Eugene Debrah <debraheug@gmail.com>
2022-04-24 21:13:41 +0000

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const { screens } = require("tailwindcss/defaultTheme");
+const { screens, borderRadius } = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
@@ -6,7 +6,36 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      xxxsm: "200px",
+      xxsm: "340px",
+      xsm: "480px",
+      ...screens,
+    },
+    extend: {
+      colors: {},
+      fontSize: {
+        "portrait-sm": "16px",
+      },
+      height: {
+        "portfolio-h-nav": "10%",
+        "portfolio-h-heror": "90%",
+        "5/2": "40%",
+      },
+      width: {
+        "portfolio-px-270": "270px",
+        "portfolio-px-300": "300px",
+        "portfolio-px-350": "350px",
+        "portfolio-px-400": "400px",
+        "portfolio-px-124": "124px",
+      },
+      borderRadius: {
+        "portfolio-px-20": "20px",
+      },
+      fontFamily: {
+        "portfolio-poppins": "Poppins",
+      },
+    },
   },
   plugins: [],
 };
