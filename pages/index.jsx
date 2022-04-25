47b357943071c462930a3635cd3ca557aca67a78
Eugene Debrah <debraheug@gmail.com>
2022-04-25 11:42:42 +0000

    ..

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,10 +2,12 @@ import Head from "next/head";
 import Nav from "../components/NavBar/Nav";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { CustomCursor } from "../components/CustomCursor";
 
 export default function Home() {
   return (
     <>
+      <CustomCursor />
       <Head>
         <title>4eug Portfolio</title>
         <meta name="description" content="Developed by Me" />
