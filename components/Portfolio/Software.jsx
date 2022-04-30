68908484c6bf76c23dffce07c66beadcdc4a71c7
Eugene Debrah <debraheug@gmail.com>
2022-04-30 14:28:54 +0000

    Portfolio Components

---
+++ components/Portfolio/Software.jsx
@@ -0,0 +1,16 @@
+const SoftwareProjects = () => {
+  return (
+    <div>
+      <div className="grid md:grid-cols-2 max-w-[1240px] m-auto">
+        <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
+          <p className="text-xl text-purple-900 mt-10 md:mt-0 ">Portfolio</p>
+          <h1 className="py-3 text-3xl md:text-3xl font-bold ">
+            What I’m building / Have built
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SoftwareProjects;
