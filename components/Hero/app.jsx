79b39ca0d5bb1811c2c8c90737fe999442d5f876
Eugene Debrah <debraheug@gmail.com>
2022-04-30 08:23:14 +0000

    ..

--- components/Hero/app.jsx
+++
@@ -1,9 +0,0 @@
-import Spline from "@splinetool/react-spline";
-
-export default function App() {
-  return (
-    <div>
-      <Spline scene="https://prod.spline.design/UHKfuoFV4Ujx-5qo/scene.spline" />
-    </div>
-  );
-}
