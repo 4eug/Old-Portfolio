c9d67ecbaafc54e33c96363d8b1f89271b01920c
Eugene Debrah <debraheug@gmail.com>
2022-05-01 18:58:05 +0000

    ...

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,6 +8,7 @@ import SideBar from "../components/NavBar/sideBar";
 import Hero from "../components/Hero/Hero";
 import SoftwareProjects from "../components/Portfolio/Software";
 import DesignProjects from "../components/Portfolio/Design";
+import About from "../components/About/About";
 
 export default function Home() {
   const [sideBar, setSideBar] = useState(false);
@@ -26,6 +27,9 @@ export default function Home() {
       <Hero />
       <SoftwareProjects />
       <DesignProjects />
+      <section className="px-8 lg:px-64 pb-8">
+        <About />
+      </section>
       <SideBar sideBar={sideBar} />
     </>
   );
