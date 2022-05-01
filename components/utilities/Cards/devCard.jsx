079917cc76c83d765f9bc7934445611b4670f2a6
Eugene Debrah <debraheug@gmail.com>
2022-05-01 18:17:43 +0000

    Added Card

---
+++ components/utilities/Cards/devCard.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+
+const Cards = (props) => {
+  return (
+    <div className="w-full py-[10rem] px-4 bg-white">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
+        <div className="max-w-sm bg-white rounded-3xl border border-gray-200 shadow-md dark:border-gray-200">
+          <div className="pt-4 pl-4 pr-4">
+            <a href="#">
+              <img className="rounded-t-lg" src="/Background.png" alt="" />
+            </a>
+          </div>
+
+          <div className="p-5">
+            <a href="#">
+              <h5 className="text-lg font-bold tracking-tight text-gray-900 ">
+                MakeWay App
+              </h5>
+            </a>
+            <p className="font-normal text-gray-700 ">
+              Mobile Development & Product Design
+            </p>
+            <p className="font-light text-gray-700 ">
+              The quick, brown fox jumps over a lazy dog. DJs flock by when MTV
+              ax quiz prog. Junk MTV quiz g nymphs. Waltz, bad nymph, for quick
+              jigs vex! Fox
+            </p>
+          </div>
+        </div>
+        <div className="max-w-sm bg-white rounded-3xl border border-gray-200 shadow-md dark:border-gray-200">
+          <div className="pt-4 pl-4 pr-4">
+            <a href="#">
+              <img className="rounded-t-lg" src="/Background.png" alt="" />
+            </a>
+          </div>
+
+          <div className="p-5">
+            <a href="#">
+              <h5 className="text-lg font-bold tracking-tight text-gray-900 ">
+                MakeWay App
+              </h5>
+            </a>
+            <p className="font-normal text-gray-700 ">
+              Mobile Development & Product Design
+            </p>
+            <p className="font-light text-gray-700 ">
+              The quick, brown fox jumps over a lazy dog. DJs flock by when MTV
+              ax quiz prog. Junk MTV quiz g nymphs. Waltz, bad nymph, for quick
+              jigs vex! Fox
+            </p>
+          </div>
+        </div>
+        <div className="max-w-sm bg-white rounded-3xl border border-gray-200 shadow-md dark:border-gray-200">
+          <div className="pt-4 pl-4 pr-4">
+            <a href="#">
+              <img className="rounded-t-lg" src="/Background.png" alt="" />
+            </a>
+          </div>
+
+          <div className="p-5">
+            <a href="#">
+              <h5 className="text-lg font-bold tracking-tight text-gray-900 ">
+                MakeWay App
+              </h5>
+            </a>
+            <p className="font-normal text-gray-700 ">
+              Mobile Development & Product Design
+            </p>
+            <p className="font-light text-gray-700 ">
+              The quick, brown fox jumps over a lazy dog. DJs flock by when MTV
+              ax quiz prog. Junk MTV quiz g nymphs. Waltz, bad nymph, for quick
+              jigs vex! Fox
+            </p>
+          </div>
+        </div>
+        <div className="max-w-sm bg-white rounded-3xl border border-gray-200 shadow-md dark:border-gray-200">
+          <div className="pt-4 pl-4 pr-4">
+            <a href="#">
+              <img className="rounded-t-lg" src="/Background.png" alt="" />
+            </a>
+          </div>
+
+          <div className="p-5">
+            <a href="#">
+              <h5 className="text-lg font-bold tracking-tight text-gray-900 ">
+                MakeWay App
+              </h5>
+            </a>
+            <p className="font-normal text-gray-700 ">
+              Mobile Development & Product Design
+            </p>
+            <p className="font-light text-gray-700 ">
+              The quick, brown fox jumps over a lazy dog. DJs flock by when MTV
+              ax quiz prog. Junk MTV quiz g nymphs. Waltz, bad nymph, for quick
+              jigs vex! Fox
+            </p>
+          </div>
+        </div>
+        <div className="max-w-sm bg-white rounded-3xl border border-gray-200 shadow-md dark:border-gray-200">
+          <div className="pt-4 pl-4 pr-4">
+            <a href="#">
+              <img className="rounded-t-lg" src="/Background.png" alt="" />
+            </a>
+          </div>
+
+          <div className="p-5">
+            <a href="#">
+              <h5 className="text-lg font-bold tracking-tight text-gray-900 ">
+                MakeWay App
+              </h5>
+            </a>
+            <p className="font-normal text-gray-700 ">
+              Mobile Development & Product Design
+            </p>
+            <p className="font-light text-gray-700 ">
+              The quick, brown fox jumps over a lazy dog. DJs flock by when MTV
+              ax quiz prog. Junk MTV quiz g nymphs. Waltz, bad nymph, for quick
+              jigs vex! Fox
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
