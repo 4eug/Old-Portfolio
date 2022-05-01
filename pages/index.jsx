b9b4829cd83e4ea525ae37f0aceae986cf51762b
Eugene Debrah <debraheug@gmail.com>
2022-05-01 18:26:20 +0000

    ...

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,6 +7,7 @@ import { CustomCursor } from "../components/CustomCursor";
 import SideBar from "../components/NavBar/sideBar";
 import Hero from "../components/Hero/Hero";
 import SoftwareProjects from "../components/Portfolio/Software";
+import DesignProjects from "../components/Portfolio/Design";
 
 export default function Home() {
   const [sideBar, setSideBar] = useState(false);
@@ -24,6 +25,7 @@ export default function Home() {
       </section>
       <Hero />
       <SoftwareProjects />
+      <DesignProjects />
       <SideBar sideBar={sideBar} />
     </>
   );
