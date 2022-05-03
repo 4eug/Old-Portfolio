a0fce20a61679ccd853aefa92681123d25400fbb
Eugene Debrah <debraheug@gmail.com>
2022-05-03 08:07:09 +0000

    ...

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,7 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
-// import { Spline } from "@splinetool/react-spline";
-// const Spline = require("@splinetool/react-spline");
 
 const Hero = () => {
   return (
