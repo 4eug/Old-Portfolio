27e3fc54f51c6d015230bb9869263b942ed2f08a
Eugene Debrah <debraheug@gmail.com>
2022-05-03 08:12:35 +0000

    Get In Touch Section

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,6 +9,7 @@ import Hero from "../components/Hero/Hero";
 import SoftwareProjects from "../components/Portfolio/Software";
 import DesignProjects from "../components/Portfolio/Design";
 import About from "../components/About/About";
+import GetInTouch from "../components/GetInTouch/getInTouch";
 
 export default function Home() {
   const [sideBar, setSideBar] = useState(false);
@@ -28,6 +29,7 @@ export default function Home() {
       <SoftwareProjects />
       <DesignProjects />
       <About />
+      <GetInTouch />
       <SideBar sideBar={sideBar} />
     </>
   );
