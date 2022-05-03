a2ad070470759e3663c4b69d8c2757e35825a979
Eugene Debrah <debraheug@gmail.com>
2022-05-03 08:01:24 +0000

    My Self

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -1,66 +1,44 @@
-// import Image from "next/image";
-// import React from "react";
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+// import { Spline } from "@splinetool/react-spline";
+// const Spline = require("@splinetool/react-spline");
 
-// function About(props) {
-//   return (
-//     <div className="mt-32 w-full h-screen flex">
-//       <div
-//         className={`flex ${
-//           props.flexDirection ?? "flex-col"
-//         } w-full bg-slate-40 justify-center gap-10
-//       lg:gap-28
-//       lg:h-portfolio-px-450
-//       ${props.lgFlexDirection ?? "lg:flex-row"}
+const Hero = () => {
+  return (
+    <div className=" flex flex-col justify-between">
+      <div className="grid md:grid-cols-2 max-w-[1240px] m-auto">
+        <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
+          <p className="text-xl text-purple-900 mt-10 md:mt-0 ">About Me</p>
+          <h1 className="py-3 text-2xl md:text-3xl font-bold ">MySelf</h1>
+          <p className="mt-2 text-md">
+            The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax
+            quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick
+            quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymphs
+            grab quick-jived waltz. Brick quiz whangs jumpy veldt fox. Bright
+            vixens jump; dozy fowl quack. Quick wafting zephyrs vex bold Jim.
+            Quick zephyrs blow, vexing daft Jim. Sex-charged fop blew my junk TV
+            quiz. How quickly daft jumping zebras vex. Two driven jocks help fax
+            my big quiz. Quick, Baz, get my woven flax jodhpurs! Now fax quiz
+            Jack! my brave ghost pled. Five quacking zephyrs jolt my wax bed.
+            Flummoxed by job, kvetching W. zaps Iraq. Cozy sphinx waves quart
+            jug of bad milk. A very bad quack might jinx zippy fowls. Few quips
+            galvanized the mock jury box. Quick brown dogs jump over the lazy
+            fox. The jay, pig, fox, zebra, and my wolves quack! Blowzy red
+            vixens fight for a quick jump. Joaquin Phoenix was gazed by MTV for
+            luck. A wizard’s job is to vex chumps quickly in fog. Watch , Alex
+            fun TV quiz game.
+          </p>
+        </div>
+        <div className="ml-0 pl-0 md:ml-10 md:pl-10">
+          {/* <div>
+            <Spline scene="https://prod.spline.design/UHKfuoFV4Ujx-5qo/scene.spline" />
+          </div> */}
 
-//       `}
-//       >
-//         <div className="w-full flex items-center">
-//           <div className="p-32">
-//             <h3 className="text-xl text-purple-900 mt-10 md:mt-0 ">My Self</h3>
-//             <h5 className="text-gray-600 mt-5">
-//               The quick, brown fox jumps over a lazy dog. DJs flock by when MTV
-//               ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick
-//               quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox
-//               nymphs grab quick-jived waltz. Brick quiz whangs jumpy veldt fox.
-//               Bright vixens jump; dozy fowl quack. Quick wafting zephyrs vex
-//               bold Jim. Quick zephyrs blow, vexing daft Jim. Sex-charged fop
-//               blew my junk TV quiz. How quickly daft jumping zebras vex. Two
-//               driven jocks help fax my big quiz. Quick, Baz, get my woven flax
-//               jodhpurs!my brave ghost pled. Five quacking zephyrs jolt my wax
-//               bed. Flummoxed by job, kvetching W. zaps Iraq. Cozy sphinx waves
-//               quart jug of bad milk. A very bad quack might jinx zippy fowls.
-//               Few quips galvanized the mock jury box. Quick brown dogs jump over
-//               the lazy fox. The jay, pig, fox, zebra, and my wolves quack!
-//               Blowzy red vixens fight for a quick jump. Joaquin Phoenix was
-//               gazed by MTV for luck. A wizard’s job is to vex chumps quickly in
-//               fog. Watch Alex fun TV quiz game.
-//             </h5>
-//           </div>
-//         </div>
+          <img className="w-full" src="/Images.png" alt="/" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-//         <div className="w-full">
-//           <div className="h-full w-full relative hidden lg:block">
-//             <Image
-//               layout="fill"
-//               objectFit="contain"
-//               priority
-//               src="/Images.png"
-//               alt="Web-Image"
-//             />
-//           </div>
-//           <div className="h-portfolio-px-450 w-full relative lg:hidden">
-//             <Image
-//               layout="fill"
-//               objectFit="contain"
-//               priority
-//               src="/Images.png"
-//               alt="Web-Image"
-//             />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default About;
+export default Hero;
