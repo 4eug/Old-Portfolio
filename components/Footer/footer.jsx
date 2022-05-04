b7fa6a3b24f3dbb79a585b584d6d916f6f26d701
Eugene Debrah <debraheug@gmail.com>
2022-05-04 11:54:08 +0000

    ...

---
+++ components/Footer/footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer className="pl-32">
+      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        © 2022{" "}
+        <a href="https://flowbite.com" className="hover:underline">
+          Flowbite™
+        </a>
+        . All Rights Reserved.
+      </span>
+    </footer>
+  );
+}
+export default Footer;
