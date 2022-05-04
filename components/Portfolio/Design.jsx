8965722b268ba3d24446287011accc7e57a2464b
Eugene Debrah <debraheug@gmail.com>
2022-05-04 16:38:01 +0000

    ...

--- components/Portfolio/Design.jsx
+++ components/Portfolio/Design.jsx
@@ -1,20 +1,23 @@
 import DesignCard from "../utilities/Cards/designCard";
+import { Fade } from "react-awesome-reveal";
 
 const DesignProjects = () => {
   return (
-    <div>
-      <div className="grid max-w-[1240px] m-auto">
-        <div className=" flex-col justify-center md:items-start w-full px-2 py-8">
-          <p className="text-xl text-purple-900 mt-10 md:mt-0 ">Portfolio</p>
-          <h1 className="py-3 text-3xl md:text-3xl font-bold ">
-            Case Studies & Projects
-          </h1>
-          <div className="flex flex-row gap-10">
-            <DesignCard />
+    <Fade damping={3}>
+      <div>
+        <div className="grid max-w-[1240px] m-auto">
+          <div className=" flex-col justify-center md:items-start w-full px-2 py-8">
+            <p className="text-xl text-purple-900 mt-10 md:mt-0 ">Portfolio</p>
+            <h1 className="py-3 text-3xl md:text-3xl font-bold ">
+              Case Studies & Projects
+            </h1>
+            <div className="flex flex-row gap-10">
+              <DesignCard />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Fade>
   );
 };
 
