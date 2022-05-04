c05700c9cc3574199aeff72463e130e26ff7839a
Eugene Debrah <debraheug@gmail.com>
2022-05-04 09:31:57 +0000

    Get In Touch Section

--- components/GetInTouch/getInTouch.jsx
+++ components/GetInTouch/getInTouch.jsx
@@ -1,5 +1,19 @@
 function GetInTouch() {
-  return <div>jdfkhjhlh</div>;
+  return (
+    <div className="mt-20 p-4 md:pl-10 md:pr-60 flex flex-col justify-between">
+      <div className="m-auto">
+        <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
+          <p className="text-xl text-purple-900 mt-10 md:mt-0 ">Say Hello</p>
+          <h1 className="py-3 text-2xl md:text-4xl font-bold ">Get In Touch</h1>
+          <p className="mt-2 text-md">
+            The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax
+            quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick
+          </p>
+          <p className="mt-4 text-lg">debraheug@gmail.com</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default GetInTouch;
