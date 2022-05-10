afe0a62b57dc389085bca7bf9cf50cebd46a965f
Eugene Debrah <debraheug@gmail.com>
2022-05-09 23:58:28 -1200

    ...

--- components/CustomCursor/index.jsx
+++ components/CustomCursor/index.jsx
@@ -4,12 +4,16 @@ export const CustomCursor = () => {
   const cursorRef = React.useRef(null);
 
   React.useEffect(() => {
-    document.addEventListener("mousemove", (event) => {
+    const onMouseMove = (e) => {
       const { clientX, clientY } = event;
       const mouseX = clientX - cursorRef.current.clientWidth / 2;
       const mouseY = clientY - cursorRef.current.clientHeight / 2;
       cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-    });
+    };
+    document.addEventListener("mousemove", onMouseMove);
+    return () => {
+      document.removeEventListener("mousemove", onMouseMove);
+    };
   }, []);
   return <div className="app-cursor" ref={cursorRef} />;
 };
