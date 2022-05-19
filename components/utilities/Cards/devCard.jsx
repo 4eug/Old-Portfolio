284e50c200905c1be3a96cab76ac08dceaedde5a
Eugene Debrah <debraheug@gmail.com>
2022-05-19 01:40:54 -1200

    ...

--- components/utilities/Cards/devCard.jsx
+++ components/utilities/Cards/devCard.jsx
@@ -1,113 +1,41 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
+import DevMiniCard from "../miniCards/devMiniCards";
 
 const DevCards = (props) => {
+  const devArray = [
+    { title: "Design", 
+      subtitle: "Mobile Development & Product Design",
+      description: "The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz g nymphs. Waltz, bad nymph, for quick jigs vex! Fox",
+     id: 1 },
+    { title: "Music",
+     subtitle: "Music & Audio",
+      description: "The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz g nymphs. Waltz, bad nymph, for quick jigs vex! Fox",
+       id: 2 },
+    { title: "Software",
+     subtitle: "Software Development",
+      description: "The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz g nymphs. Waltz, bad nymph, for quick jigs vex! Fox",
+       id: 3 },
+    { title: "Dance",
+     subtitle: "Dance & Movement",
+      description: "The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz g nymphs. Waltz, bad nymph, for quick jigs vex! Fox",
+       id: 4 },
+       { title: "Lifestyle",
+     subtitle: "Lifestyle & Travel",
+      description: "The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz g nymphs. Waltz, bad nymph, for quick jigs vex! Fox",
+       id: 5 },
+  ]
   return (
     <div className="w-full px-4 py-8 ">
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
-        <div className="max-w-sm  rounded-3xl border border-gray-200 shadow-md dark:border-gray-200">
-          <div className="pt-4 pl-4 pr-4">
-            <a href="#">
-              <img className="rounded-t-lg" src="/devbg.png" alt="" />
-            </a>
-          </div>
-
-          <div className="p-5">
-            <a href="#">
-              <h5 className="text-lg font-bold tracking-tight  ">
-                MakeWay App
-              </h5>
-            </a>
-            <p className="font-normal  ">Mobile Development & Product Design</p>
-            <p className="font-light  ">
-              The quick, brown fox jumps over a lazy dog. DJs flock by when MTV
-              ax quiz prog. Junk MTV quiz g nymphs. Waltz, bad nymph, for quick
-              jigs vex! Fox
-            </p>
-          </div>
-        </div>
-        <div className="max-w-sm  rounded-3xl border border-gray-200 shadow-md dark:border-gray-200">
-          <div className="pt-4 pl-4 pr-4">
-            <a href="#">
-              <img className="rounded-t-lg" src="/devbg.png" alt="" />
-            </a>
-          </div>
-
-          <div className="p-5">
-            <a href="#">
-              <h5 className="text-lg font-bold tracking-tight  ">
-                MakeWay App
-              </h5>
-            </a>
-            <p className="font-normal  ">Mobile Development & Product Design</p>
-            <p className="font-light  ">
-              The quick, brown fox jumps over a lazy dog. DJs flock by when MTV
-              ax quiz prog. Junk MTV quiz g nymphs. Waltz, bad nymph, for quick
-              jigs vex! Fox
-            </p>
-          </div>
-        </div>
-        <div className="max-w-sm  rounded-3xl border border-gray-200 shadow-md dark:border-gray-200">
-          <div className="pt-4 pl-4 pr-4">
-            <a href="#">
-              <img className="rounded-t-lg" src="/devbg.png" alt="" />
-            </a>
-          </div>
-
-          <div className="p-5">
-            <a href="#">
-              <h5 className="text-lg font-bold tracking-tight ">MakeWay App</h5>
-            </a>
-            <p className="font-normal  ">Mobile Development & Product Design</p>
-            <p className="font-light  ">
-              The quick, brown fox jumps over a lazy dog. DJs flock by when MTV
-              ax quiz prog. Junk MTV quiz g nymphs. Waltz, bad nymph, for quick
-              jigs vex! Fox
-            </p>
-          </div>
-        </div>
-        <div className="max-w-sm  rounded-3xl border border-gray-200 shadow-md dark:border-gray-200">
-          <div className="pt-4 pl-4 pr-4">
-            <a href="#">
-              <img className="rounded-t-lg" src="/devbg.png" alt="" />
-            </a>
-          </div>
-
-          <div className="p-5">
-            <a href="#">
-              <h5 className="text-lg font-bold tracking-tight  ">
-                MakeWay App
-              </h5>
-            </a>
-            <p className="font-normal  ">Mobile Development & Product Design</p>
-            <p className="font-light  ">
-              The quick, brown fox jumps over a lazy dog. DJs flock by when MTV
-              ax quiz prog. Junk MTV quiz g nymphs. Waltz, bad nymph, for quick
-              jigs vex! Fox
-            </p>
-          </div>
-        </div>
-        <div className="max-w-sm  rounded-3xl border border-gray-200 shadow-md dark:border-gray-200">
-          <div className="pt-4 pl-4 pr-4">
-            <a href="#">
-              <img className="rounded-t-lg" src="/devbg.png" alt="" />
-            </a>
-          </div>
-
-          <div className="p-5">
-            <a href="#">
-              <h5 className="text-lg font-bold tracking-tight  ">
-                MakeWay App
-              </h5>
-            </a>
-            <p className="font-normal  ">Mobile Development & Product Design</p>
-            <p className="font-light  ">
-              The quick, brown fox jumps over a lazy dog. DJs flock by when MTV
-              ax quiz prog. Junk MTV quiz g nymphs. Waltz, bad nymph, for quick
-              jigs vex! Fox
-            </p>
-          </div>
-        </div>
+        {devArray.map((item) => (
+        <DevMiniCard
+        title={item.title}
+            subtitle={item.subtitle}
+            description={item.description}
+            key={item.id}
+        />
+        ))}
       </div>
     </div>
   );
