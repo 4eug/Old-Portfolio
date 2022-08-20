30aff4ea981c124c763afa0e2e6b3caa95b0c5b7
4eug <debraheug@gmail.com>
2022-08-20 15:22:22 +0000

    Footer

--- components/Footer/footer.jsx
+++ components/Footer/footer.jsx
@@ -1,10 +1,10 @@
 function Footer() {
   return (
     <footer className="p-4 md:pl-80 md:pr-80">
-      <hr className="my-6 border-gray-200 mx-auto sm:mx-auto dark:border-gray-300 lg:my-8" />
-      <span className="block text-sm  text-center sm:text-center ">
+      <span className="block text-base text-center sm:text-center ">
         Copyright &copy;
-        <a href="#">4eug</a> {new Date().getFullYear()}
+        <a href="#">4eug</a> {new Date().getFullYear()}{' '}
+         All rights reserved 
       </span>
     </footer>
   );
