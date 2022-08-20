0504689cd04ac0b342f810ed2de01fd4e3819e69
4eug <debraheug@gmail.com>
2022-08-20 11:38:12 +0000

    About Page

---
+++ pages/about.jsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import Nav from "../components/NavBar/Nav";
+import { useState } from "react";
+import { CustomCursor } from "../components/CustomCursor";
+import SideBar from "../components/NavBar/sideBar";
+import Meta from "../components/utilities/meta/meta";
+
+export default function Home() {
+  const [sideBar, setSideBar] = useState(false);
+  return (
+    <>
+      <CustomCursor />
+      <Meta title="About Me" />
+      <Head>
+        <meta name="description" content="Developed by Me" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <section>
+        <Nav sideBar={sideBar} setSideBar={setSideBar} />
+      </section>
+      <SideBar sideBar={sideBar} />
+    </>
+  );
+}
