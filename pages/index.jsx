288902f63d43a61cd5d742ab715c4a88bd5ef0c1
4eug <debraheug@gmail.com>
2022-08-20 17:53:20 +0000

    Console Data

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Nav from "../components/NavBar/Nav";
-import { useState } from "react";
+import React, { useState } from "react";
 import { CustomCursor } from "../components/CustomCursor";
 import SideBar from "../components/NavBar/sideBar";
 import Hero from "../components/Hero/Hero";
@@ -12,6 +12,19 @@ import Meta from "../components/utilities/meta/meta";
 
 export default function Home() {
   const [sideBar, setSideBar] = useState(false);
+
+  React.useEffect(() => {
+    console.log.apply(console, [
+      "%c Designed and Developed by Eugene Debrah  🚀 \n",
+      "color: #fff; background: #ff5833; padding:5px 0;",
+    ]);
+    console.log.apply(console, [
+      "%c You Opened the Console, Thanks for checking.\n",
+      "color: #fff; background: #ff5833; padding:5px 0;",
+    ]);
+  }, [])
+
+
   return (
     <>
       <CustomCursor />
