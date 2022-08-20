23552f591d3a8e24a2899f79b17d8e96da4106cc
4eug <debraheug@gmail.com>
2022-08-20 14:12:06 +0000

    About Page

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -18,6 +18,15 @@ const About = () => {
           <h3 className="text-4xl md:text-6xl text-center mt-2 leading-16 tracking-tight font-normal">
           experiences.
           </h3>
+          <div className="flex justify-center items-center">
+          <p className='mt-10 text-start text-lg max-w-xl'>
+          a <span className="font-bold">Software Developer</span>(Mobile & Front-end) and <span className="font-bold">Product Designer </span>
+          who is passionate about building and designing beautiful 
+          and value-based applications or digital 
+          products for businesses.
+              </p>
+          </div>
+          
           <button className="black text-white mt-20 mb-10">
           CONNECT WITHME
           </button>
