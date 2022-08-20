7ee9089a4b5c240232efb0b38bef3c856590e451
4eug <debraheug@gmail.com>
2022-08-20 19:41:36 +0000

    Change in footer

--- components/Footer/footer.jsx
+++ components/Footer/footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     <footer className="p-4 md:pl-80 md:pr-80">
       <span className="block text-sm text-center sm:text-center ">
         Copyright &copy;
-        <a href="#">4eug</a> {new Date().getFullYear()}{' '}
+        <a href="#">Eugene(4eug)</a> {new Date().getFullYear()}{' '}
          All rights reserved 
       </span>
     </footer>
