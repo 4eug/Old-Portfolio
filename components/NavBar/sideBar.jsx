1c6f0e2bf8647230bbf33d61c230c9815bd1f73b
4eug <debraheug@gmail.com>
2022-08-20 15:20:04 +0000

    Side bar

--- components/NavBar/sideBar.jsx
+++ components/NavBar/sideBar.jsx
@@ -15,46 +15,14 @@ const SideBar = (props) => {
             exit={{
               y: "-100%",
             }}
-            transition={{ type: "spring", bounce: 0, duration: 0.5 }}
-            className="fixed shadow-lg top-0 right-0 w-full p-5 text-right flex flex-col items-end gap-10 z-30"
+            transition={{ type: "spring", bounce: 0, duration: 0.2 }}
+            className="fixed shadow-lg top-0 right-0 w-full p-2 text-right flex flex-col items-end gap-10 z-30"
           >
-            <div className="m-auto">
+            <div className="m-auto m">
               <ul className="flex flex-col items-center gap-10">
-                <a href="mailto:debraheug@gmail.com">
+                <a href="/about">
                   <button className="white">
-                    <span>Let’s build</span>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="28.732"
-                      height="16.616"
-                      viewBox="0 0 20.732 18.616"
-                      className="inline-block"
-                    >
-                      <g id="Message" transform="translate(0.75 0.75)">
-                        <path
-                          id="Path_445"
-                          d="M11.314,0,7.048,3.434a2.223,2.223,0,0,1-2.746,0L0,0"
-                          transform="translate(3.954 5.561)"
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeMiterlimit="10"
-                          strokeWidth="1.5"
-                        />
-                        <path
-                          id="Rectangle_511"
-                          d="M4.888,0h9.428A4.957,4.957,0,0,1,17.9,1.59a5.017,5.017,0,0,1,1.326,3.7v6.528a5.017,5.017,0,0,1-1.326,3.7,4.957,4.957,0,0,1-3.58,1.59H4.888C1.968,17.116,0,14.741,0,11.822V5.294C0,2.375,1.968,0,4.888,0Z"
-                          transform="translate(0 0)"
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeMiterlimit="10"
-                          strokeWidth="1.5"
-                        />
-                      </g>
-                    </svg>
+                    About Me 
                   </button>
                 </a>
               </ul>
