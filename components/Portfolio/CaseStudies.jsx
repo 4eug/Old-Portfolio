7cfbd20f27bf943054cda65199ee0da56c911bb4
4eug <debraheug@gmail.com>
2022-08-21 11:08:21 +0000

    Case Studies

--- components/Portfolio/CaseStudies.jsx
+++ components/Portfolio/CaseStudies.jsx
@@ -3,74 +3,9 @@ import Chips from "../utilities/Buttons/chips";
 import { motion } from "framer-motion";
 
 /* eslint-disable @next/next/no-html-link-for-pages */
-export const Content = () => {
-    const chipsContent1 = [
-      {
-        names:"Website",
-        imgSrc:"/website.svg",
-        href:"/",
-        id: 0
-      },
-      {
-        names:"PlayStore",
-        imgSrc:"/playstore.svg",
-        href:"/",
-        id: 1
-      },
-      {
-        names:"AppStore",
-        imgSrc:"/appstore.svg",
-        href:"/about",
-        id: 2
-      }
-    ]
-    const chipsContent2 = [
-      {
-        names:"PlayStore",
-        imgSrc:"/playstore.svg",
-        href:"/",
-        id: 0
-      }, 
-    ]
-    const chipsContent3 = [
-      {
-        names:"PlayStore",
-        imgSrc:"/playstore.svg",
-        href:"/",
-        id: 0
-      },
-      {
-        names:"Github",
-        imgSrc:"/github.svg",
-        href:"/",
-        id: 1
-      },
-    ]
-    const chipsContent4 = [
-      {
-        names:"Website",
-        imgSrc:"/website.svg",
-        href:"/",
-        id: 0
-      }, 
-      {
-        names:"Github",
-        imgSrc:"/github.svg",
-        href:"/",
-        id: 1
-      }, 
-    ]
-    const chipsContent5 = [
-      {
-        names:"Website",
-        imgSrc:"/website.svg",
-        href:"/",
-        id: 0
-      }, 
-    ]
-
+export const CaseStudies = () => {
     return (
-      <div className="mt-24 px-4 py-16 md:ml-24  sm:max-w-xl md:max-w-full lg:max-w-screen-xl xl:max-w-screen-2xl md:px-24 lg:px-8 lg:py-20">
+      <div className="px-4 py-16 md:ml-24  sm:max-w-xl md:max-w-full lg:max-w-screen-xl xl:max-w-screen-2xl md:px-24 lg:px-8 lg:py-20">
         <div className="grid max-w-screen-xl xl:max-w-screen-2xl  m-auto">
           <div className=" flex-col justify-center md:items-start w-full px-2 py-8">
             
@@ -98,7 +33,7 @@ export const Content = () => {
           
         </div>
         <div className="mt-16 mb-10 flex justify-center items-center">
-        <a href="https://github.com/4eug" target="_blank" rel="noreferrer">
+        <a href="" target="_blank" rel="noreferrer">
             <motion.button
               whileHover={{ scale: 1.05 }}
               className="black  md:block"
