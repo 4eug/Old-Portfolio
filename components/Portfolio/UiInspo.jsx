16c5c6f645f10acf718fb65e3b7a32ad625d938b
4eug <debraheug@gmail.com>
2022-08-21 14:08:00 +0000

    Ui Inspo

---
+++ components/Portfolio/UiInspo.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable @next/next/no-html-link-for-pages */
+import Marquee from "react-fast-marquee";
+
+export const UiInspo = () => {
+    return (
+      <div className="px-4 py-16 md:ml-24 sm:max-w-xl md:max-w-full lg:max-w-screen-xl xl:max-w-screen-2xl md:px-24 lg:px-8 lg:py-20">
+        <div className="grid max-w-screen-xl xl:max-w-screen-2xl  m-auto">
+          <div className=" flex-col justify-center md:items-start w-full px-2 py-8">
+            
+            <h1 className="py-3 text-3xl md:text-5xl font-semibold ">
+                UI Design –
+                Inspiro
+            </h1>
+            <Marquee gradient={false}>
+                <div className="flex gap-1 md:gap-2 lg:grid-cols-3">
+                
+                    <li><img src="https://cdn.dribbble.com/users/5224484/screenshots/15988541/media/f90b4f62e6d59798f8c3cc195bf6cc8b.png?compress=1&resize=800x600"></img></li>
+                            <li><img src="https://cdn.dribbble.com/users/5224484/screenshots/15411817/media/9b7b78c377019596c1a38fe73603aa67.jpg?compress=1&resize=800x600"></img></li>
+                          <li><img src="https://cdn.dribbble.com/users/5224484/screenshots/15184253/media/53988a6cd8a8c7060ff682fadc3debe2.jpg?compress=1&resize=800x600"></img></li>
+                          <li><img src="https://cdn.dribbble.com/users/5224484/screenshots/14869287/shot-cropped-1609774629202.png?compress=1&resize=800x600"></img></li>
+                          <li><img src="https://cdn.dribbble.com/users/5224484/screenshots/15523874/media/326fb3371ffd05ae7455019216116ee5.jpg?compress=1&resize=800x600"></img></li>
+                          <li><img src="https://cdn.dribbble.com/users/5224484/screenshots/15411806/media/9ebcbd40c04adbcd4feca08df66a7132.jpg?compress=1&resize=800x600"></img></li>
+                          <li><img src="https://cdn.dribbble.com/users/5224484/screenshots/11256954/shot-cropped-1588494031032.png?compress=1&resize=800x600"></img></li>
+                          <li><img src="https://cdn.dribbble.com/users/5224484/screenshots/15157088/media/d3fc2c4e816236d28932816050b946e3.jpg?compress=1&resize=800x600"></img></li>
+                          <li><img src="https://cdn.dribbble.com/users/5224484/screenshots/15157122/media/5bb7b082113d7032a4b27010ddfdacdf.jpg?compress=1&resize=800x600"></img></li>
+                          <li><img src="https://cdn.dribbble.com/users/5224484/screenshots/16068299/media/414b7e91b0df77211a3a50a3e8130994.png?compress=1&resize=800x600"/></li>
+                          <li><img src="https://cdn.dribbble.com/users/5224484/screenshots/16372190/media/92a57ab411ada4cbdf5c6099ab9d79b4.png?compress=1&resize=800x600"/></li>
+                          <li><img src="https://cdn.dribbble.com/users/5224484/screenshots/16372112/media/c87e8dfb17ee58bc53629d3187daf69e.png?compress=1&resize=800x600"/></li>
+                          <li><img src="https://cdn.dribbble.com/users/5224484/screenshots/16181073/media/130a882cb8338f0d82406664e0070efc.png?compress=1&resize=800x600"/></li>
+                           
+                    
+                </div>
+            </Marquee>
+
+          </div>
+        </div>
+       
+        
+      </div>
+    );
+  };
