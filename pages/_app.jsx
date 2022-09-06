fdfe44676e63c8a1f2b99d4149b50725b209e458
4eug <debraheug@gmail.com>
2022-09-06 19:35:38 +0000

    Page Transition

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,38 @@
 import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
-import { AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 
-function MyApp({ Component, pageProps, router }) {
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <ThemeProvider>
       <AnimatePresence mode="wait">
-      <Component {...pageProps} key={router.route} />
+        <motion.div
+        key={router.route}
+        initial="initialState"
+        animate="animateState"
+        exit="exitState"
+        transition={{
+          duration: 1,
+        }}
+        variants={{
+          initialState: {
+            opacity: 0,
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+          },
+          animateState: {
+            opacity: 1,
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+          },
+          exitState: {
+            clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+          },
+        }}
+        className="base-page-size"
+        >
+      <Component {...pageProps} />
+      </motion.div>
       </AnimatePresence>
     </ThemeProvider>
   );
