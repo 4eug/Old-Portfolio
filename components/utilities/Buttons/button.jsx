e23522eeb37a2e5a410141efa43aff83d32b5afc
4eug <debraheug@gmail.com>
2022-09-06 19:35:47 +0000

    Page Transition

--- components/utilities/Buttons/button.jsx
+++ components/utilities/Buttons/button.jsx
@@ -1,16 +1,19 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 function AboutButton() {
   return (
-    <a href="/about">
-      <motion.button
-        whileHover={{ scale: 1.05 }}
-        className="white hidden md:block"
-      >
-        <span className="font-medium text-base">About Me</span>
-      </motion.button>
-    </a>
+    <Link href="/about">
+    <a>
+    <motion.button
+      whileHover={{ scale: 1.05 }}
+      className="white hidden md:block"
+    >
+      <span className="font-medium text-base">About Me</span>
+    </motion.button>
+  </a>
+  </Link>
   );
 }
 
