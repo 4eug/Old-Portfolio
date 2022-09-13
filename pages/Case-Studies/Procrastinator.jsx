bc30bc3ea466f3d996f0473646524aae9eabac67
4eug <debraheug@gmail.com>
2022-09-13 08:10:26 +0000

    Case Studies [1]

---
+++ pages/Case-Studies/Procrastinator.jsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import MiniNav from "../../components/NavBar/miniNav";
+import { useState } from "react";
+import { CustomCursor } from "../../components/CustomCursor";
+import SideBar from "../../components/NavBar/sideBar";
+import Meta from "../../components/utilities/meta/meta";
+import About from "../../components/About/About";
+import Footer from "../../components/Footer/footer";
+
+export default function Home() {
+  const [sideBar, setSideBar] = useState(false);
+  return (
+    <>
+      <CustomCursor />
+      <Meta title="Procrastinator" />
+      <Head>
+        <meta name="description" content="Developed by Me" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <section>
+        <MiniNav sideBar={sideBar} setSideBar={setSideBar} />
+      </section>
+      <Footer/>
+      <SideBar sideBar={sideBar} />
+    </>
+  );
+}
