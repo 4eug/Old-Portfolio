79af6c76d3499a7c09aab28395e06aebc296fd5b
4eug <debraheug@gmail.com>
2022-09-13 20:41:00 +0000

    User Flow

---
+++ components/CaseStudies/Procrastinator/user-flow.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Image from 'next/image'
+
+function UserFlow() {
+  return (
+    <div className="mt-4 text-center"> 
+      <h6 className="text-3xl md:text-5xl text-white font-semibold">Userflow Map</h6>
+        <div className="flex justify-center items-center">
+            <p className='mt-2 text-center text-base text-white md:text-base max-w-4xl'>
+            The Userflow map shows how users will navigate around the app
+            </p>
+        </div>
+        <div className="mt-10 m-auto flex justify-center items-center">
+            <Image
+            src="/user-flow.svg"
+            width="375"
+            height="949"
+            priority
+            alt=""
+            />    
+        </div>
+    </div>
+  )
+}
+
+export default UserFlow;
