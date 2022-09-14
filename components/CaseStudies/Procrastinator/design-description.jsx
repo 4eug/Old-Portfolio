ab0a0f5d8499c082dfa1c5dc4aa682fc469fdf2a
4eug <debraheug@gmail.com>
2022-09-14 16:43:13 +0000

    changes

--- components/CaseStudies/Procrastinator/design-description.jsx
+++ components/CaseStudies/Procrastinator/design-description.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function DesignDescription() {
   return (
     <div>
-        <div className="grid ml-2 md:ml-32 ">
+        <div className="grid ml-2 md:ml-32 2xl:ml-72">
         <div className="flex flex-col justify-center md:items-start 2xl:pl-2 2xl:mb-2 w-full px-2 py-8 ">
           <h1 className="text-xl md:text-3xl font-bold">
           Design
