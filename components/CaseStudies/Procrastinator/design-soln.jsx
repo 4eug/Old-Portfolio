138413d1e022a355f5bb6700475119555694bc42
4eug <debraheug@gmail.com>
2022-09-14 16:43:19 +0000

    changes

--- components/CaseStudies/Procrastinator/design-soln.jsx
+++ components/CaseStudies/Procrastinator/design-soln.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function DesignSoln() {
   return (
     <div>
-        <div className="mt-4 m-auto flex justify-center items-center">
+        <div className="m-auto flex justify-center items-center">
             <Image
             src="/design-soln.svg"
             width="1440"
