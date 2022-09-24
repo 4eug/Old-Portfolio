641f07bea160c741a574dbb647ed3c4acd8f75ec
4eug <debraheug@gmail.com>
2022-09-24 22:28:58 +0000

    Design Content

---
+++ components/Designs/Designs.jsx
@@ -0,0 +1,56 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import {motion} from "framer-motion";
+import WebsiteBtn from "../utilities/Buttons/website";
+import PlaystoreBtn from "../utilities/Buttons/playstore";
+import AppstoreBtn from "../utilities/Buttons/appstore";
+import GithubBtn from "../utilities/Buttons/github";
+import DesignContents from "./content";
+
+let easing = [0.6, -0.05, 0.01, 0.99];
+
+const stagger = {
+  animate: {
+    transition: {
+      staggerChildren: 0.05
+    }
+  }
+};
+
+const fadeInUp = {
+  initial: {
+    y: 60,
+    opacity: 0,
+    transition: { duration: 0.6, ease: easing }
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+      ease: easing
+    }
+  }
+};
+
+const Designs = () => {
+  return (
+    <motion.section initial='initial' animate='animate' exit={{ opacity: 0 }} className="pt-8 md:pt-20">
+        <div variants={fadeInUp} className="text-center flex gap-3 justify-center"> 
+          <h6 className="mt-8 text-[36px] md:text-[50px]">Designs</h6>
+          <img className="mt-10 md:mt-12 h-[38px] w-[38px] md:h-[45px] md:w-[45px]" src="/Code.svg" alt="/" />
+        </div>
+        <div className="flex justify-center items-center">
+          <p className='mt-4 text-center text-lg max-w-2xl'>
+          I’ve built and worked on applications and websites 
+            using anything from HTML to React Flutter and Nodejs etc.Here are some 
+            of my favorite projects over the course of my learning journey.
+              </p>
+          </div>
+            <DesignContents/>
+      </motion.section>
+  );
+};
+
+export default Designs;
---
+++ components/Designs/content.jsx
@@ -0,0 +1,79 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+import React from 'react'
+import {motion} from "framer-motion";
+import Link from 'next/link';
+
+export default function DesignContents() {
+    const designs =[
+        {
+         imgSrc: "/Background.png",
+         url: "/projects",
+         id: 0
+        },
+        {
+          imgSrc: "/Background.png",
+          url: "/about",
+          id: 1
+        }
+    ]
+  return (
+    <div className="flex justify-center items-center">
+  <div className="2xl:mx-auto 2xl:container lg:px-20 lg:py-16 md:py-12 md:px-6 py-9 px-4 w-96 sm:w-auto">
+    <div className="lg:flex items-stretch md:mt-12 mt-8">
+      <div className="lg:w-1/2">
+        <div className="sm:flex items-center justify-between xl:gap-x-8 gap-x-6">
+            {designs.map((item) => (
+                <Link key={item.id} href={item.url} passHref>
+                <motion.div 
+                whileHover={{ scale: 1.1 }}
+                transition={{ type: "spring", stiffness: 200, damping: 10 }}
+                className="sm:w-2/3 sm:mt-0 mt-4 relative" key={item.id}>
+                    <img src={item.imgSrc} className="w-full h-full rounded-3xl border-2" />        
+                </motion.div>
+                </Link>
+            ))}
+        </div>
+
+        {/* Design Post 3 */}
+        <Link href="/Case-Studies/Procrastinator" passHref >
+        <motion.div 
+        whileHover={{ scale: 1.1 }}
+        transition={{ type: "spring", stiffness: 200, damping: 10 }}
+        className="relative">
+          <img src="/procrastinator-image.svg" alt="sitting place" className="w-full mt-8 md:mt-6 rounded-3xl border-2" />
+        </motion.div>
+        </Link>
+
+
+      </div>
+      <div className="lg:w-1/2 xl:ml-8 lg:ml-4 lg:mt-0 md:mt-6 mt-4 lg:flex flex-col justify-between">
+        {/* Design Post 4 */}
+        <Link href="/" passHref>
+        <motion.div
+        whileHover={{ scale: 1.1 }}
+        transition={{ type: "spring", stiffness: 200, damping: 10 }}
+        className="relative">
+          <img src="/MakeWay.jpeg" alt="makeway" className="w-full rounded-3xl border-2" />
+        </motion.div>
+        </Link>
+
+        {/* Design Post 5 */}
+        <div className="sm:flex items-center justify-between xl:gap-x-8 gap-x-6 md:mt-6 mt-4">
+        <Link href="/" passHref>
+          <motion.div 
+          whileHover={{ scale: 1.1 }}
+          transition={{ type: "spring", stiffness: 200, damping: 10 }}
+          className="relative w-full sm:mt-0 mt-4">
+            <img src="/AdinkraIcons.png" className="w-full h-full rounded-3xl" alt="wall design" />
+          </motion.div>
+          </Link>
+        </div>
+
+
+      </div>
+    </div>
+  </div>
+</div>
+  )
+}
