5a989e6edc6f5936b3185ba4eeeb7240a9585cff
4eug <debraheug@gmail.com>
2022-10-09 19:04:21 +0000

    Hero Section Chnaged

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <motion.section animate={{ x: 0, opacity: 1 }}
     initial={{ x: 200, opacity: 0 }}
     exit={{ opacity: 0 }}
-    transition={{ delay: 0.2 }} className="mt-16 md:mt-40">
+    transition={{ delay: 0.2 }} className="mt-16 md:mt-40 2xl:mt-44 2xl:mb-72">
     <div className="py-8 px-4 mx-auto w-[344px] h-[284px] md:w-[1157px] md:h-[237px] text-center lg:py-16 lg:px-12">
        
         <h1 className="text-4xl md:text-6xl text-center mt-8 leading-16 tracking-tight font-normal">
