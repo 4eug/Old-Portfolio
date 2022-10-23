4517329b8bca032c5897cf56ace36c41f2a2a50a
4eug <debraheug@gmail.com>
2022-10-23 05:30:43 +0000

    connect btn

--- components/utilities/Buttons/connect-btn.jsx
+++ components/utilities/Buttons/connect-btn.jsx
@@ -5,9 +5,9 @@ function ConnectButton() {
     <a href="mailto:debraheug@gmail.com">
       <motion.button
         whileHover={{ scale: 1.05 }}
-        className="black  md:block"
+        className="black  md:block hover:bg-black hover:text-white"
       >
-        <span className="font-medium ">CONNECT WITHME</span>
+        <span className="font-medium ">CONNECT WITH ME</span>
       </motion.button>
     </a>
   );
