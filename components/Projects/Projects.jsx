dc80b91ea41f466f0dd66e4f6a7972c2afe33ddf
4eug <debraheug@gmail.com>
2022-10-24 11:48:50 +0000

    GhData added

--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -181,6 +181,24 @@ const Projects = () => {
               </div>
             </div>
 
+            <div className="flex flex-col h-full">
+              <img
+                src="/Ghdata.png"
+                className="object-cover w-[360px] ml-2 md:ml-auto md:w-full h-48 rounded-2xl duration-500 hover:scale-105"
+                alt=""
+              />
+              <div className="">
+                <div className="pt-4">
+                    <div className="flex gap-2 lg:grid-cols-3">
+                   <WebsiteBtn href="https://gh-data.herokuapp.com/"/>
+                    </div>
+                  <h2 className="mt-4 text-xl leading-5 ml-2 md:ml-auto">
+                  GhData API
+                  </h2>
+                </div>
+              </div>
+            </div>
+
             {/* <div className="flex flex-col h-full">
               <img
                 src=""
