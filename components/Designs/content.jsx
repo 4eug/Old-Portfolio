5d8e66654f482c3994f1a49486fe8045581ed4ab
4eug <debraheug@gmail.com>
2022-10-30 17:46:53 +0000

    Designs Page

--- components/Designs/content.jsx
+++ components/Designs/content.jsx
@@ -4,67 +4,115 @@ import React from 'react'
 import Link from 'next/link';
 
 export default function DesignContents() {
-    const designs =[
-        {
-         imgSrc: "/Background.png",
-         url: "/projects",
-         id: 0
-        },
-        {
-          imgSrc: "/Background.png",
-          url: "/about",
-          id: 1
-        }
-    ]
+    
   return (
-    <div className="flex justify-center items-center">
-  <div className="2xl:mx-auto 2xl:container lg:px-20 lg:py-16 md:py-12 md:px-6 py-9 px-4 w-96 sm:w-auto">
-    <div className="lg:flex items-stretch md:mt-12 mt-8">
-      <div className="lg:w-1/2">
-        <div className="sm:flex items-center justify-between xl:gap-x-8 gap-x-6">
-            {designs.map((item) => (
-                <Link key={item.id} href={item.url} passHref>
-                <div 
-                className="sm:w-2/3 sm:mt-0 mt-4 relative" key={item.id}>
-                    <img src={item.imgSrc} className="w-full h-full rounded-3xl border-2 duration-500 hover:scale-105" />        
-                </div>
-                </Link>
-            ))}
-        </div>
-
-        {/* Design Post 3 */}
-        <Link href="/Case-Studies/Procrastinator" passHref >
-        <div 
-        className="relative">
-          <img src="/procrastinator-image.svg" alt="sitting place" className="w-full mt-8 md:mt-6 rounded-3xl border-2 duration-500 hover:scale-105" />
-        </div>
-        </Link>
-
-
-      </div>
-      <div className="lg:w-1/2 xl:ml-8 lg:ml-4 lg:mt-0 md:mt-6 mt-4 lg:flex flex-col justify-between">
-        {/* Design Post 4 */}
-        <Link href="/" passHref>
-        <div
-        className="relative">
-          <img src="/MakeWay.jpeg" alt="makeway" className="w-full rounded-3xl border-2 duration-500 hover:scale-105" />
-        </div>
-        </Link>
-
-        {/* Design Post 5 */}
-        <div className="sm:flex items-center justify-between xl:gap-x-8 gap-x-6 md:mt-6 mt-4">
-        <Link href="/" passHref>
-          <div 
-          className="relative w-full sm:mt-0 mt-4">
-            <img src="/AdinkraIcons.png" className="w-full h-full rounded-3xl duration-500 hover:scale-105" alt="wall design" />
-          </div>
+    <div className="mt-2 px-4 py-16  sm:max-w-xl md:max-w-full lg:px-[130px] lg:py-20">
+    <div className="grid gap-10 row-gap-5 mb-8 lg:grid-cols-3 sm:grid-cols-2">
+     
+        <div className="flex flex-col h-full">
+          <Link href="/Case-Studies/Procrastinator" passHref>
+          <img
+            src="/Procrastinator.jpeg"
+            className="object-cover w-[360px] ml-2 md:ml-auto md:w-full h-48 rounded-2xl duration-500 hover:scale-105"
+            alt=""
+          />
           </Link>
+          <div className="">
+            <div className="pt-4">
+              <h2 className="mt-4 text-xl leading-5 ml-2 md:ml-auto">
+              Procrastinator
+              </h2>
+              <div className="flex gap-1 md:gap-2 lg:grid-cols-3">
+                 
+                </div>
+            </div>
+          </div>
+        </div>
+      
+      
+        <div className="flex flex-col h-full">
+          <Link href="" passHref>
+          <img
+            src="/MakeWay.jpeg"
+            className="object-cover w-[360px] ml-2 md:ml-auto md:w-full h-48 rounded-2xl duration-500 hover:scale-105"
+            alt=""
+          />
+          </Link>
+          <div className="">
+            <div className="pt-4">
+              <h2 className="mt-4 text-xl leading-5 ml-2 md:ml-auto">
+              MakeWay App
+              </h2>
+              <div className="flex gap-1 md:gap-2 lg:grid-cols-3">
+                 
+                </div>
+            </div>
+          </div>
+        </div>
+      
+        <div className="flex flex-col h-full">
+          <Link href="https://www.behance.net/gallery/151783469/Buy-Sell-and-Design-3D-Icons-UI-Design" passHref>
+          <img
+            src="/Header.png"
+            className="object-cover w-[360px] ml-2 md:ml-auto md:w-full h-48 rounded-2xl duration-500 hover:scale-105"
+            alt=""
+          />
+          </Link>
+          <div className="">
+            <div className="pt-4">
+              <h2 className="mt-4 text-xl leading-5 ml-2 md:ml-auto">
+              Maxicons
+              </h2>
+              <div className="flex gap-1 md:gap-2 lg:grid-cols-3">
+                 
+                </div>
+            </div>
+          </div>
         </div>
 
+        <div className="flex flex-col h-full">
+          <Link href="" passHref>
+          <img
+            src="/AdinkraIcons.png"
+            className="object-cover w-[360px] ml-2 md:ml-auto md:w-full h-48 rounded-2xl duration-500 hover:scale-105"
+            alt=""
+          />
+          </Link>
+          <div className="">
+            <div className="pt-4">
+              <h2 className="mt-4 text-xl leading-5 ml-2 md:ml-auto">
+              Vintage Item Header
+              </h2>
+              <div className="flex gap-1 md:gap-2 lg:grid-cols-3">
+                 
+                </div>
+            </div>
+          </div>
+        </div>
 
-      </div>
+        <div className="flex flex-col h-full">
+          <Link href="https://www.behance.net/gallery/148806355/Brandfolio-2019-2022" passHref>
+          <img
+            src="/BrandFolio.jpg"
+            className="object-cover w-[360px] ml-2 md:ml-auto md:w-full h-48 rounded-2xl duration-500 hover:scale-105"
+            alt=""
+          />
+          </Link>
+          <div className="">
+            <div className="pt-4">
+              <h2 className="mt-4 text-xl leading-5 ml-2 md:ml-auto">
+              BrandFolio 2018 - 2021
+              </h2>
+              <div className="flex gap-1 md:gap-2 lg:grid-cols-3">
+                 
+                </div>
+            </div>
+          </div>
+        </div>
+
+        
+      
     </div>
-  </div>
-</div>
+    </div>
   )
 }
