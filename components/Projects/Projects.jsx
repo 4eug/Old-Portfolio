87a4516e73dcda30c8af346d7937f1f2a74b498c
4eug <debraheug@gmail.com>
2022-11-19 19:41:48 +0000

    projects

--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -201,14 +201,14 @@ const Projects = () => {
 
             <div className="flex flex-col h-full">
               <img
-                src="/Oya!.png"
+                src="/oya-cover.png"
                 className="object-cover w-[360px] ml-2 md:ml-auto md:w-full h-48 rounded-2xl duration-500 hover:scale-105"
                 alt=""
               />
               <div className="">
                 <div className="pt-4">
                     <div className="flex gap-2 lg:grid-cols-3">
-                   {/* <WebsiteBtn href="https://famtechnologiesgh.com/"/> */}
+                   <WebsiteBtn href=""/>
                     </div>
                   <h2 className="mt-4 text-xl leading-5 ml-2 md:ml-auto">
                   Oya! Ghana Website
